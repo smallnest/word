5a8f54d6c091acc35c2f257af1cff96df37e83f6
smallnest <smallnest@gmail.com>
2019-03-11 12:08:37 +0800

    some words have not been translated into Chinese

--- cambridge.go
+++ cambridge.go
@@ -10,6 +10,7 @@ import (
 )
 
 var cambridgeURL = "https://dictionary.cambridge.org/dictionary/english-chinese-simplified/"
+var cambridgeEnURL = "https://dictionary.cambridge.org/dictionary/english/"
 
 type cambridge struct{}
 
@@ -41,8 +42,8 @@ func (e cambridge) audio(word string, us bool) (mp3, ipa, def string, err error)
 	} else { //uk
 		val, ok := doc.Find(".uk span.audio_play_button").Attr("data-src-mp3")
 		if !ok {
-			fmt.Println("failed to get audio")
-			return mp3, ipa, def, errors.New("not found")
+			//fmt.Println("failed to get audio")
+			return e.englishAudio(word, us)
 		}
 
 		mp3 = "https://dictionary.cambridge.org" + val
@@ -55,5 +56,48 @@ func (e cambridge) audio(word string, us bool) (mp3, ipa, def string, err error)
 	})
 
 	ipa = "[" + ipa + "]"
+	return mp3, ipa, def, nil
+}
+
+func (e cambridge) englishAudio(word string, us bool) (mp3, ipa, def string, err error) {
+	u := cambridgeEnURL + word
+	resp, err := http.Get(u)
+	if err != nil {
+		fmt.Printf("failed to get audio from cambridge: %v\n", err)
+		return mp3, ipa, def, err
+	}
+	defer resp.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		fmt.Printf("failed to read response from cambridge: %v\n", err)
+		return mp3, ipa, def, err
+	}
+
+	if us {
+		val, ok := doc.Find(".us span.audio_play_button").Attr("data-src-mp3")
+		if !ok {
+			fmt.Println("failed to get audio")
+			return mp3, ipa, def, errors.New("not found")
+		}
+
+		mp3 = "https://dictionary.cambridge.org" + val
+
+		ipa = doc.Find(".us span.ipa").Text()
+	} else { //uk
+		val, ok := doc.Find(".uk span.audio_play_button").Attr("data-src-mp3")
+		if !ok {
+			fmt.Println("failed to get audio")
+			return mp3, ipa, def, errors.New("not found")
+		}
+
+		mp3 = "https://dictionary.cambridge.org" + val
+
+		ipa = doc.Find(".uk span.ipa").Text()
+	}
+
+	def = doc.Find("p.def-head b.def").Text()
+	ipa = "[" + ipa + "]"
+
 	return mp3, ipa, def, nil
 }
